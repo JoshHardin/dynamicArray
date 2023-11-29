aad4a90453e40d653c4d2769fb505259d3c8abfe
Jasmine <jasminetruong97@gmail.com>
2023-11-29 15:46:15 -0800

    unshift

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,44 +1,61 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here 
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(this.capacity)
   }
 
   read(index) {
-
-    // Your code here 
+    return this.data[index]
   }
 
   push(val) {
-
-    // Your code here 
+    this.data[this.length] = val
+    this.length++
   }
 
 
   pop() {
-
-    // Your code here 
+    // if array has nothing, return undefined
+    if(this.length === 0) return undefined
+    //ele is last element of your array
+    let ele = this.data[this.length-1]
+    //delete that element from array
+    delete this.data[this.length-1]
+    //update the length
+    this.length--
+    //return removed element
+    return ele
   }
 
   shift() {
-
-    // Your code here 
+    if(this.length === 0) return undefined
+    let ele = this.data[0]
+    delete this.data[0]
+    for(let i = 0; i < this.length; i++){
+      this.data[i] = this.data[i+1]
+    }
+    this.length--
+    return ele
   }
 
   unshift(val) {
-
-    // Your code here 
+    for(let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i-1]; //shifts all the values over one slot to the right
+    }
+    this.data[0] = val //this data[0] is the same as this data[1] but we rewrite this.data[0] to be val passed in
+    this.length++
   }
 
   indexOf(val) {
 
-    // Your code here 
+    // Your code here
   }
 
   resize() {
 
-    // Your code here 
+    // Your code here
   }
 
 }
